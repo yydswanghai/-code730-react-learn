e2d64c68bd7482ccc4e9fa4337aea7b3cb70f770
wanghai <2275981101@qq.com>
2023-05-04 10:32:46 +0800

    事件案例

--- react-learn-5/src/index2.js
+++ react-learn-5/src/index2.js
@@ -6,6 +6,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 function handleClick(e) {
     console.log('点击了！！！', e)
 }
+/**
+ * <button></button> 是react给你封装好的一个基础组件，直接使用onClick会在合适的时候触发
+ */
 
 const btn = <button onClick={handleClick} onMouseEnter={(e) => {
     console.log('鼠标移入了', e)
