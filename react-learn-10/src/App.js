ab7c12e4e92842710af9098521a52228880acfa4
wanghai <2275981101@qq.com>
2023-05-11 15:06:31 +0800

    组件练习

--- react-learn-10/src/App.js
+++ react-learn-10/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Test from './components/Test'
+// import Test from './components/Test'
+import FormTest from './components/FormTest'
 
 export default function App() {
     return (
         <div className='root'>
-            <Test />
+            <FormTest />
         </div>
     )
 }
