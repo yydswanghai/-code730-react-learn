b124402374f9601f72b8d16e9708531bceef5ae1
wanghai <2275981101@qq.com>
2023-05-23 15:09:52 +0800

    ref

---
+++ 11-ref/practice/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Comp from './components/Comp'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className='app'>
+                <Comp />
+            </div>
+        )
+    }
+}
---
+++ 11-ref/practice/src/components/Comp.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+export default class Comp extends Component {
+
+    state = {
+        show: true
+    }
+
+    handleClick = () => {
+        this.setState({
+            show: !this.state.show
+        })
+    }
+
+    componentDidMount(){
+        console.log('didMount', this.text)
+    }
+
+    getRef = el => {
+        console.log('getRef函数被调用了', el)
+        this.txt = el;
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.state.show && <input ref={this.getRef} type="text" />
+                }
+                <button onClick={this.handleClick}>显示/隐藏</button>
+            </div>
+        )
+    }
+}
