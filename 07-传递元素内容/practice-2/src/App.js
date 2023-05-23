ff524b9663e02f60c16e6d1d5f234638f65be300
wanghai <2275981101@qq.com>
2023-05-23 11:18:28 +0800

    传递元素内容

--- 07-传递元素内容/practice-2/src/App.js
+++ 07-传递元素内容/practice-2/src/App.js
@@ -1,10 +1,5 @@
 import React from 'react'
-// import Test from './components/Test'
-// import FormTest from './components/Form/FormTest'
-// import FormTest2 from './components/Form/FormTest2'
-// import Test from './components/CheckBoxGroup/Test'
-// import Test from './components/RadioBoxGroup/Test'
-import Test from './components/Select/Test'
+import Test from './components/Test'
 
 export default function App() {
     return (
