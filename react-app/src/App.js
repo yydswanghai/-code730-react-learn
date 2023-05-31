80b75d8ad258f5635c7a48fec4d7209ec2574d09
wanghai <2275981101@qq.com>
2023-05-31 16:01:50 +0800

    react-app

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,35 +2,36 @@ import React, { Component } from 'react'
 import moment from 'moment'
 import Select from './components/Select'
 
-function getTime(h = '08', m = '50') {
-    return moment(`${moment().format('YYYY-MM-DD')} ${h}:${m}`)
+function setMoment(h, m, s = 0) {
+    return moment().hours(h).minute(m).second(s)
 }
-function diff(start, end) {
-    const minute = end.diff(start, 'minute');
-    return `距离下班还有: ${Math.floor(minute/60)}小时 ${Math.floor(minute%60)}分`
+// 根据结束时分秒计算倒计时
+function getCountDown(end_h, end_m, end_s) {
+    const start = moment();// 当前时间
+    const end = setMoment(end_h, end_m, end_s);
+
+    const diff = end.diff(start, 'seconds');
+    const duration = moment.duration(diff, 'seconds');
+    return `距离落班还有: ${duration.hours()}时 ${duration.minutes()}分 ${duration.seconds()}秒`
 }
-function add(startH, startM) {
-    const delay = 570;
-    const start = getTime(startH, startM);
-    const result = start.add(delay, 'm');
-    return {
-        h: result.format('HH'),
-        m: result.format('mm')
-    }
+function getStr(start_h, start_m) {
+    const delay = (9*60 + 30) * 60;// 秒
+    const start = setMoment(start_h, start_m);
+    const end = start.add(delay, 'seconds');
+    return getCountDown(end.hours(), end.minute(), end.second())
 }
+
 const local_key = '__clock-off__';
 export default class App extends Component {
 
     state = {
-        curTime: {
-            hour: moment().format('HH'),
-            minute: moment().format('mm')
-        },
         start: {
             hour: '08',
             minute: '30',
         }
     }
+
+    timer = null;
 
     componentDidMount(){
         const storage = JSON.parse(sessionStorage.getItem(local_key));
@@ -42,46 +43,62 @@ export default class App extends Component {
                 }
             })
         }
+        this.autoTiming()
+    }
+
+    autoTiming(){
+        clearInterval(this.timer)
+        this.timer = setInterval(() => {
+            this.setState({})
+        }, 1000);
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timer)
+    }
+
+    handleChangeSelect = (newVal, type) => {
+        if(type === 'hours'){
+            this.setState({
+                start: {
+                    ...this.state.start,
+                    hour: newVal
+                }
+            }, () => {
+                sessionStorage.setItem(local_key, JSON.stringify({
+                    hour: this.state.start.hour,
+                    minute: this.state.start.minute,
+                }))
+            })
+        }else{
+            this.setState({
+                start: {
+                    ...this.state.start,
+                    minute: newVal
+                }
+            }, () => {
+                sessionStorage.setItem(local_key, JSON.stringify({
+                    hour: this.state.start.hour,
+                    minute: this.state.start.minute,
+                }))
+            })
+        }
     }
 
     render() {
-        const { h, m } = add(this.state.start.hour, this.state.start.minute)
-        const time = diff(
-            getTime(this.state.curTime.hour, this.state.curTime.minute),
-            getTime(h, m));
+        console.log('render')
+        const str = getStr(this.state.start.hour, this.state.start.minute)
         return (
             <div className='app'>
                 <p>
-                    <Select curMinute={this.state.start.minute} curHour={this.state.start.hour} onChange={(newVal, type) => {
-                        if(type === 'hours'){
-                            this.setState({
-                                start: {
-                                    ...this.state.start,
-                                    hour: newVal
-                                }
-                            }, () => {
-                                sessionStorage.setItem(local_key, JSON.stringify({
-                                    hour: this.state.start.hour,
-                                    minute: this.state.start.minute,
-                                }))
-                            })
-                        }else{
-                            this.setState({
-                                start: {
-                                    ...this.state.start,
-                                    minute: newVal
-                                }
-                            }, () => {
-                                sessionStorage.setItem(local_key, JSON.stringify({
-                                    hour: this.state.start.hour,
-                                    minute: this.state.start.minute,
-                                }))
-                            })
-                        }
-                    }} />
+                    上班时间：<Select
+                        curMinute={this.state.start.minute}
+                        curHour={this.state.start.hour}
+                        onChange={this.handleChangeSelect}
+                        />
                 </p>
                 <p>
-                    {time}
+                    {str}
                 </p>
             </div>
         )
