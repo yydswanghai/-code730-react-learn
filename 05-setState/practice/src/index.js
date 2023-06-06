d031f6264bf30592e6f92717d2540efbdf7bffd0
wanghai <2275981101@qq.com>
2023-06-06 11:06:50 +0800

    setState

--- 05-setState/practice/src/index.js
+++ 05-setState/practice/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Comp from './Comp4'
+// import Comp from './Comp3'
+// import Comp from './Comp2'
 // import Comp from './Comp'
-import Comp4 from './Comp4'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Comp4 />)
+root.render(<Comp />)
