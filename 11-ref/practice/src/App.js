f52de79c23ee7fe9026bd93069c8b4f26181b100
wanghai <2275981101@qq.com>
2023-06-06 11:36:37 +0800

    ref

--- 11-ref/practice/src/App.js
+++ 11-ref/practice/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+// import Comp from './components/Comp3'
+// import Comp from './components/Comp2'
 import Comp from './components/Comp'
 
 export default class App extends Component {
--- 11-ref/practice2/src/index.js
+++ 11-ref/practice2/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App'
-// import App from './App2'
 import App from './App3'
+// import App from './App2'
+// import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
