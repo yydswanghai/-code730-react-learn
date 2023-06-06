2a60ce3ef8df3676cc5ae7a0add45632e4efa0af
wanghai <2275981101@qq.com>
2023-06-06 11:04:59 +0800

    事件

--- 04-事件/practice/src/index.js
+++ 04-事件/practice/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import TickControl from './TickControl3'
+import TickControl from './components/TickControl3'
+// import TickControl from './components/TickControl2'
+// import TickControl from './components/TickControl'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
