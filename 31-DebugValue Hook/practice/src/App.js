87a622d1205bb356edff990842790bf6ba27a33d
wanghai <2275981101@qq.com>
2023-06-15 17:28:09 +0800

    DebugValue Hook

---
+++ 31-DebugValue Hook/practice/src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState, useDebugValue } from 'react'
+
+function useTest() {
+    const [students, ] = useState([]);
+    useDebugValue('学生集合')
+    return students;
+}
+
+export default function App() {
+    useState(0)
+    useState('abc')
+    useEffect(() => {
+        console.log('effect')
+    }, [])
+    useTest();
+
+    return (
+        <div>
+        </div>
+    )
+}
