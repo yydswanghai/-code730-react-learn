b4e06f888739fab0b307618435b756f4c9c24231
wanghai <2275981101@qq.com>
2023-07-03 10:47:51 +0800

    工具

--- 19-工具/practice/src/mock/index.js
+++ 19-工具/practice/src/mock/index.js
@@ -4,10 +4,14 @@ mock.setup({ timeout: '200-600' })
 
 export async function getStudentsList() {
     return await mock.mock({
+        code: 200,
         'data|1-10': [{
             'id|+1': 1,
             'name': '@cname',
-            'age|10-50': 18
-        }]
+            'age|10-50': 18,
+            'email': '@email',
+            'birth': '@date(yyyy-MM-dd HH:mm)',
+        }],
+        msg: 'ok'
     })
 }
