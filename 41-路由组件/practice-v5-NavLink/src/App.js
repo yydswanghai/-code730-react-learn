e5a0c17de592f01769bb46aad003603703241ff7
wanghai <2275981101@qq.com>
2023-07-10 14:59:34 +0800

    v5 NavLink

---
+++ 41-路由组件/practice-v5-NavLink/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+import './App.css'
+
+const PageA = () => <h1>PageA</h1>;
+const PageB = () => <h1>PageB</h1>;
+
+function NavBar() {
+    return <div>
+        <NavLink to="/a" exact strict activeClassName="selected" activeStyle={{ background: '#eee' }}>nav-a</NavLink>
+        <br/>
+        <NavLink to={{
+            pathname: '/b',
+            hash: '#abc',
+            search: '?a=1&b=2'
+        }} replace innerRef={node => {
+            console.log(node)
+        }}>nav-b</NavLink>
+    </div>
+}
+
+export default function App() {
+    return (
+        <Router>
+            <NavBar />
+            <Route path="/a" component={PageA} />
+            <Route path="/b" component={PageB} />
+        </Router>
+    )
+}
