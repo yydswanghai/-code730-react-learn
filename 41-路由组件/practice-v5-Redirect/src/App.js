db8c2c5071e48cccd99ccc58f928100f3f9cbb98
wanghai <2275981101@qq.com>
2023-07-10 15:05:52 +0800

    v5 Redirect

---
+++ 41-路由组件/practice-v5-Redirect/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch, NavLink, Redirect } from 'react-router-dom'
+
+const PageA = () => <h1>PageA</h1>;
+const PageB = () => <h1>PageB</h1>;
+
+function NavBar() {
+    return <div>
+        <NavLink to="/a" exact strict activeStyle={{ background: '#eee' }}>nav-a</NavLink>
+        <br/>
+        <NavLink to="/b">nav-b</NavLink>
+        <br/>
+        <NavLink to="/abc/1">other</NavLink>
+    </div>
+}
+
+export default function App() {
+    return (
+        <Router>
+            <NavBar />
+            <Switch>
+                <Route path="/a" component={PageA} />
+                <Route path="/b" component={PageB} />
+                <Redirect from="/abc/:id" to="/a/:id" />
+            </Switch>
+        </Router>
+    )
+}
