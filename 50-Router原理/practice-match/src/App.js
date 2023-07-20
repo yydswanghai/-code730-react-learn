0daa81f3f017babb68be54bad39af37c52d80aae
wanghai <2275981101@qq.com>
2023-07-20 09:34:32 +0800

    match

---
+++ 50-Router原理/practice-match/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import matchPath from './react-router/matchPath'
+
+console.log(matchPath('/news/:id/:page', '/news/42/abc'))
+
+export default function App() {
+    return <div>
+    </div>
+}
