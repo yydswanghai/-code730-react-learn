48ed1232e437b813d62fd7dfb96079380a1e2033
wanghai <2275981101@qq.com>
2023-08-30 11:31:07 +0800

    redux-promise

---
+++ 57-redux-promise/practice/src/store/test/index.js
@@ -0,0 +1,11 @@
+import store from '../index'
+import { changeAction } from '../action/student/searchCondition'
+import { fetchStudents } from '../action/student/searchResult'
+
+store.dispatch(changeAction({
+    keyword: '傅'
+}))
+
+store.dispatch(
+    fetchStudents(store.getState())
+)
